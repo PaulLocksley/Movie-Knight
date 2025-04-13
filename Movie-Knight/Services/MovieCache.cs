2cbe8006d165ba4ad8f3f37a677d9efd26c08dde
Paul <paullocksley@outlook.com>
2025-04-13 18:05:21 +1000

    Update MovieCache.cs

--- Movie-Knight/Services/MovieCache.cs
+++ Movie-Knight/Services/MovieCache.cs
@@ -16,7 +16,7 @@ public static class MovieCache
     static MovieCache()
     {
         _jt = new JsonSerializer();
-        _filePath = $"{Environment.CurrentDirectory}{Path.DirectorySeparatorChar}MovieCache.json";
+        _filePath = $"{Environment.CurrentDirectory}{Path.DirectorySeparatorChar}cache{Path.DirectorySeparatorChar}MovieCache.json";
         MovieService = new MovieService();
         Cache =  new ConcurrentDictionary<int, Movie>();
         try
